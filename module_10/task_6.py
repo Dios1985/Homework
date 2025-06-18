cd0520ec2c634d98c83000fa2e0e6d3d6e68e99e
Margo <margarita.kotova@icloud.com>
2025-06-18 19:53:42 +0300

    hi

--- module_10/task_6.py
+++ module_10/task_6.py
@@ -28,6 +28,6 @@
 
 
 height = int(input('Введите высоту пирамиды: '))
-for row in range(1, height + 1):
+for row in range(height + 1):
     spaces = ' ' * (height - row)
     print(spaces + '#' * (2 * row - 1))
