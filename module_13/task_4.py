143ce24d386d5bd0a9e3bb58adbdcc3c698470cf
Margo <margarita.kotova@icloud.com>
2025-09-17 22:32:31 +0300

    hi

--- module_13/task_4.py
+++ module_13/task_4.py
@@ -46,14 +46,21 @@
 def main():
     # TODO вам нужно только 3 функции: main, count_numbers и функция для изменения числа
     first_n = int(input("Введите первое число: "))
-    second_n = int(input("\nВведите второе число: "))
-
     first_num_count = count_numbers(first_n)
-    # TODO если в числе меньше 3 цифр, то не нужно запрашивать второе число
+
+    if first_num_count < 3:  # TODO если в числе меньше 3 цифр, то не нужно запрашивать второе число
+        print("В первом числе меньше трёх цифр.\f")
+        main()
+
+    second_n = int(input("\nВведите второе число: "))
     second_num_count = count_numbers(second_n)
 
-    first_n = change_first_number(first_n, first_num_count)
-    second_n = change_second_number(second_n, second_num_count)
+    if second_num_count < 4:
+        print("Во втором числе меньше четырёх цифр.\f")
+        main()
+
+    first_n = change_number(first_n, first_num_count, 3, 'первое')
+    second_n = change_number(second_n, second_num_count, 4, 'второе')
 
     print('\nСумма чисел:', first_n + second_n)
 
@@ -67,28 +74,14 @@ def count_numbers(count):
     return num_count
 
 
-def change_first_number(first_n, first_num_count):
-    if first_num_count < 3:
-        print("В первом числе меньше трёх цифр.")
-    else:
-        # TODO код дублируется с фунцией change_second_number
-        last_digit = first_n % 10
-        first_digit = first_n // 10 ** (first_num_count - 1)
-        between_digits = first_n % 10 ** (first_num_count - 1) // 10
-        first_n = last_digit * 10 ** (first_num_count - 1) + between_digits * 10 + first_digit
-        print('Изменённое первое число:', first_n)
-    return first_n
+def change_number(number, num_count, total_count, name):
+    # TODO код дублируется с фунцией change_second_number
+    last_digit = number % 10
+    first_digit = number // 10 ** (num_count - 1)
+    between_digits = number % 10 ** (num_count - 1) // 10
+    number = last_digit * 10 ** (num_count - 1) + between_digits * 10 + first_digit
+    print('Изменённое', name, 'число:', number)
+    return number
 
-
-def change_second_number(second_n, second_num_count):
-    if second_num_count < 4:
-        print("Во втором числе меньше четырёх цифр.")
-    else:
-        last_digit = second_n % 10
-        first_digit = second_n // 10 ** (second_num_count - 1)
-        between_digits = second_n % 10 ** (second_num_count - 1) // 10
-        second_n = last_digit * 10 ** (second_num_count - 1) + between_digits * 10 + first_digit
-        print('Изменённое второе число:', second_n)
-    return second_n
 # TODO после функции должны быть 2 пустые строчки
 main()
